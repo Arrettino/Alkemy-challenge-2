9d81b297757d666cc73fb80a3ae5efb0acebb470
Arrettino <batukearrettino@outlook.com>
2021-01-12 19:46:52 -0300

    create route '/server/posts'

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const routes = require('./api/routes/index');
 
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use('/server', routes);
 
 module.exports = app;

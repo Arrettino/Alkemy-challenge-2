6617ab66e348f4fe8a64b7d60a8d13c2f192785b
Arrettino <batukearrettino@outlook.com>
2021-01-13 11:29:27 -0300

    add database config

--- backend/config/default.js
+++ backend/config/default.js
@@ -6,4 +6,11 @@ module.exports = {
   server: {
     nodeEnv: process.env.NODE_ENV,
   },
+  db: {
+    database: process.env.DATABASE,
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    host: process.env.HOST,
+    dialect: process.env.DIALECT,
+  },
 };

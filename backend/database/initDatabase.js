4cbbdd72bb27e7d9b354ff7a3dfa87a7cd4d4073
Arrettino <batukearrettino@outlook.com>
2021-01-13 15:04:05 -0300

    chage requires of initDatabase

--- backend/database/initDatabase.js
+++ backend/database/initDatabase.js
@@ -1,18 +1,17 @@
 const config = require('config');
 const sequelize = require('./index');
-const categoriesModel = require('../app/models/categories');
-const CategoriesModel = require('../app/models/categories/CategoriesModel');
+const { findAllCategories, createCategories, CategoriesModel } = require('../app/models/categories');
 const PostsModel = require('../app/models/posts/PostsModel');
 
 async function initDatabase() {
   try {
     await sequelize.sync();
     await PostsModel.belongsTo(CategoriesModel, { as: 'categories', foreignKey: 'categoriesId' });
-    const response = await categoriesModel.findAllCategories();
+    const response = await findAllCategories();
     if (!response[0]) {
       const categories = config.get('categories');
       Object.keys(categories).forEach(async (key) => {
-        await categoriesModel.createCategories(categories[key]);
+        await createCategories(categories[key]);
       });
     }
   } catch (err) {

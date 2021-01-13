fe8dd9a5f1742b50c01aaea31a4ec0dd9044ba2f
Arrettino <batukearrettino@outlook.com>
2021-01-13 17:50:45 -0300

    create route POST 'server/posts'

--- backend/app/api/routes/posts/index.js
+++ backend/app/api/routes/posts/index.js
@@ -7,4 +7,9 @@ router.get('/', postsController.findAllPosts);
 
 router.get('/:id', postsController.findPosts);
 
+router.post('/', (req, res) => {
+  const data = req.body;
+  res.status(200).send(data);
+});
+
 module.exports = router;

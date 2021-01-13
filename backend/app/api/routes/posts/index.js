5281319f46399ea155b9b8441d1ca896dd9dcd76
Arrettino <batukearrettino@outlook.com>
2021-01-13 16:40:57 -0300

    create route GET 'server/posts/:id'

--- backend/app/api/routes/posts/index.js
+++ backend/app/api/routes/posts/index.js
@@ -5,4 +5,9 @@ const router = express.Router();
 
 router.get('/', postsController.findAllPosts);
 
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  res.status(200).send({ id });
+});
+
 module.exports = router;

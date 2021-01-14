b68d208023d3a108bd8cd88188b9bfd5396278b7
Arrettino <batukearrettino@outlook.com>
2021-01-14 15:38:53 -0300

    add attributes to the query

--- backend/app/models/posts/findAllPosts.js
+++ backend/app/models/posts/findAllPosts.js
@@ -3,12 +3,16 @@ const CategoriesModel = require('../categories/CategoriesModel');
 
 async function findAllPosts() {
   const response = await PostsModel.findAll({
+    raw: true,
+    nest: true,
     include: [
       {
         model: CategoriesModel,
+        attributes: ['id', 'name'],
         as: 'categories',
       },
     ],
+    attributes: ['id', 'title', 'image'],
   });
   return (response);
 }

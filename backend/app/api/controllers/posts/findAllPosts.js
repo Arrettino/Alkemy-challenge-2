839e51e46b95da6e4cc873670f15cf3ad82a999f
Arrettino <batukearrettino@outlook.com>
2021-01-15 00:02:12 -0300

    modificate errorHandler

--- backend/app/api/controllers/posts/findAllPosts.js
+++ backend/app/api/controllers/posts/findAllPosts.js
@@ -1,11 +1,12 @@
 const findAllPostsService = require('../../../services/posts/findAllPosts');
+const { internalError } = require('../../errors');
 
 async function findAllPosts(req, res, next) {
   try {
     const response = await findAllPostsService();
     res.status(response.status).send(response.data);
   } catch (err) {
-    next(err);
+    next(internalError());
   }
 }
 
--- backend/app/api/middlewares/errorHandler.js
+++ backend/app/api/middlewares/errorHandler.js
@@ -1,10 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 function errorhandler(error, req, res, next) {
-  res.status(500).json({
-    status: 500,
-    name: 'UnkwnownError',
-    message: 'Unkwnown Error',
-  });
+  res.status(error.status).json(error);
 }
 
 module.exports = errorhandler;

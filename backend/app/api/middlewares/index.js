eb6326e042344a195d1f6cc1cbf4c16b8b331464
Arrettino <batukearrettino@outlook.com>
2021-01-15 03:18:36 -0300

    add validationHandler into middlewares index

--- backend/app/api/middlewares/index.js
+++ backend/app/api/middlewares/index.js
@@ -1,7 +1,9 @@
 const asyncHandler = require('./asyncHandler');
 const errorHandler = require('./errorHandler');
+const validationHandler = require('./validationHandler');
 
 module.exports = {
   asyncHandler,
   errorHandler,
+  validationHandler,
 };

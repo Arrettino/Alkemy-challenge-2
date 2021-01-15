cf5ef798a8383e2184fea2a6ed3b3da914efd180
Arrettino <batukearrettino@outlook.com>
2021-01-15 05:27:53 -0300

    revalue image, createdAt in validation schema

--- backend/app/api/validations/index.js
+++ backend/app/api/validations/index.js
@@ -1,12 +1,14 @@
 const Joi = require('joi');
 
+const imgUrlRegexp = new RegExp(/(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|png)/);
+
 const schemas = {
   posts: Joi.object().keys({
     title: Joi.string().min(3).max(30).required(),
     content: Joi.string().min(3).required(),
-    image: Joi.string(),
+    image: Joi.string().regex(imgUrlRegexp).message('image must be a (jpg/png) url').required(),
     categoriesId: Joi.number().integer().positive().required(),
-    createdAt: Joi.string().length(10).required(),
+    createdAt: Joi.date().required(),
   }),
   postsId: Joi.object().keys({
     id: Joi.number().integer(),

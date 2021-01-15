159ec964fdc3debbcebaab5760907d50ebd0af2d
Arrettino <batukearrettino@outlook.com>
2021-01-15 05:54:27 -0300

    add res.send in deletePosts

--- backend/app/api/controllers/posts/deletePosts.js
+++ backend/app/api/controllers/posts/deletePosts.js
@@ -5,7 +5,7 @@ async function createPosts(req, res, next) {
   const { id } = req.params;
   const response = await deletePostsService({ id });
   if (response.database) {
-    res.status(response.status).end();
+    res.status(response.status).send(response.message);
   } else {
     next(postsNotFoundError({ id }));
   }
--- backend/app/services/posts/deletePosts.js
+++ backend/app/services/posts/deletePosts.js
@@ -2,7 +2,7 @@ const postsModel = require('../../models/posts');
 
 async function deletePosts({ id }) {
   const response = await postsModel.deletePosts({ id });
-  return ({ status: 200, database: response });
+  return ({ status: 200, message: { status: 'OK' }, database: response });
 }
 
 module.exports = deletePosts;

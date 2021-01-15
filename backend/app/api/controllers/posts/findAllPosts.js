5a58dee0dbcf4c3423b3cf58c1098d1d4020d62d
Arrettino <batukearrettino@outlook.com>
2021-01-14 23:25:52 -0300

    add try catch to findAllPosts func

--- backend/app/api/controllers/posts/findAllPosts.js
+++ backend/app/api/controllers/posts/findAllPosts.js
@@ -1,8 +1,12 @@
 const findAllPostsService = require('../../../services/posts/findAllPosts');
 
-async function findAllPosts(req, res) {
-  const response = await findAllPostsService();
-  res.status(response.status).send(response.data);
+async function findAllPosts(req, res, next) {
+  try {
+    const response = await findAllPostsService();
+    res.status(response.status).send(response.data);
+  } catch (err) {
+    next(err);
+  }
 }
 
 module.exports = findAllPosts;

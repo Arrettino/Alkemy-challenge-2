8e378213960e7cc5f734ac1828dee04deef1bd8d
Arrettino <batukearrettino@outlook.com>
2021-01-15 00:33:07 -0300

    remove next param of findAllPosts func

--- backend/app/api/controllers/posts/findAllPosts.js
+++ backend/app/api/controllers/posts/findAllPosts.js
@@ -1,6 +1,6 @@
 const findAllPostsService = require('../../../services/posts/findAllPosts');
 
-async function findAllPosts(req, res, next) {
+async function findAllPosts(req, res) {
   const response = await findAllPostsService();
   res.status(response.status).send(response.data);
 }

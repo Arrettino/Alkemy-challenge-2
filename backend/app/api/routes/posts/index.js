37e15e4465b4aeefb1739e461c14f861a18a5557
Arrettino <batukearrettino@outlook.com>
2021-01-15 03:19:49 -0300

    add validations into posts routes

--- backend/app/api/routes/posts/index.js
+++ backend/app/api/routes/posts/index.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const postsController = require('../../controllers/posts');
-const { asyncHandler } = require('../../middlewares');
+const { asyncHandler, validationHandler } = require('../../middlewares');
+const { postsId, posts } = require('../../validations');
 
 const router = express.Router();
 
 router.get('/', asyncHandler(postsController.findAllPosts));
 
-router.get('/:id', asyncHandler(postsController.findPosts));
+router.get('/:id',
+  validationHandler(postsId, 'params'),
+  asyncHandler(postsController.findPosts));
 
-router.post('/', asyncHandler(postsController.createPosts));
+router.post('/',
+  validationHandler(posts, 'body'),
+  asyncHandler(postsController.createPosts));
 
-router.patch('/:id', asyncHandler(postsController.updatePosts));
+router.patch('/:id',
+  validationHandler(postsId, 'params'),
+  validationHandler(posts, 'body'),
+  asyncHandler(postsController.updatePosts));
 
-router.delete('/:id', asyncHandler(postsController.deletePosts));
+router.delete('/:id',
+  validationHandler(postsId, 'params'),
+  asyncHandler(postsController.deletePosts));
 
 module.exports = router;
